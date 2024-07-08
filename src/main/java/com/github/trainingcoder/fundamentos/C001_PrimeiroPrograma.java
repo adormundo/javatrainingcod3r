d70e1eea6947584dee54679a8308ecdbb407d718
AlviM <adormundo@gmail.com>
2024-07-08 00:41:48 -0300

    👨🏻‍💻 Aula: 01

---
+++ src/main/java/com/github/trainingcoder/fundamentos/C001_PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package main.java.com.github.trainingcoder.fundamentos;
+
+public class C001_PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
