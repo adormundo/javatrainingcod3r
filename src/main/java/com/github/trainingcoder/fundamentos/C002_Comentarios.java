742befa9760546ed7c2b4c5aba86db02ac6f9020
JiihDev <jih.santos86@gmail.com>
2024-07-09 21:43:26 -0300

    👨🏻‍💻 Aula: 02

---
+++ src/main/java/com/github/trainingcoder/fundamentos/C002_Comentarios.java
@@ -0,0 +1,21 @@
+/**
+ * Esta classe representa exemplos de comentários em Java.
+ * @author JiihDev
+ * @since JDK 22.0
+ */
+public class Main {
+    /**
+     * Este é o método principal que inicia a execução do programa.
+     * @param args Argumentos da linha de comando.
+     */
+    public static void main(String[] args) {
+        // Exemplo de comentário em linha única
+
+        /*
+         * Exemplo de comentário
+         * em múltiplas linhas.
+         */
+
+        // System.out.println("Desistir?");
+    }
+}
