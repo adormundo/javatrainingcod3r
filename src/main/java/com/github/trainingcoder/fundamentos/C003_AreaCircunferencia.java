eadfe1d183a3a7229184fd8abe567c2ae8b0138d
AlviM <adormundo@gmail.com>
2024-07-18 23:19:47 -0300

    👨🏻‍💻 Aula: 03

---
+++ src/main/java/com/github/trainingcoder/fundamentos/C003_AreaCircunferencia.java
@@ -0,0 +1,15 @@
+package main.java.com.github.trainingcoder.fundamentos;
+
+public class C003_AreaCircunferencia {
+    public static void main(final String[] args) {
+        double raio = 3.4;
+        final double PI = 3.14159;
+
+        double area = PI * raio * raio;
+        System.out.println(area);
+
+        raio = 10;
+        area = PI * raio * raio;
+        System.out.printf("Área = " + area + "m2.");
+    }
+}
