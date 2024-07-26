004c2dbeb038339e69356a0a4b94cf9049beda56
AlviM <adormundo@gmail.com>
2024-07-26 00:32:38 -0300

    👨🏻‍💻 Aula: 23

---
+++ src/main/java/com/github/trainingcoder/controle/C030_For.java
@@ -0,0 +1,11 @@
+package main.java.com.github.trainingcoder.controle;
+
+public class C030_For {
+    public static void main(String[] args) {
+
+        for (int contador = 0; contador <= 20; contador++) {
+            System.out.println("i = " + contador);
+        }
+
+    }
+}
