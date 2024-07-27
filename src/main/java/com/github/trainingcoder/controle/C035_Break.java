4f04c4db51175cbfcc958c0cb79264ff6fc35196
AlviM <adormundo@gmail.com>
2024-07-27 00:57:52 -0300

    👨🏻‍💻 Aula: 27

---
+++ src/main/java/com/github/trainingcoder/controle/C035_Break.java
@@ -0,0 +1,16 @@
+package main.java.com.github.trainingcoder.controle;
+
+public class C035_Break {
+    public static void main(String[] args) {
+        System.out.println("Vou contar até quatro:");
+        for (int numero = 0; numero < 5; numero++) {
+            System.out.println(numero);
+
+            if (numero == 4) {
+                System.out.println("Cheguei em quatro! Hora de parar.");
+                break; // Termina o loop for quando chegar em 4
+            }
+        }
+        System.out.println("Contagem finalizada.");
+    }
+}
