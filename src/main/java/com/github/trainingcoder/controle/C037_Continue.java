7e067022806796c5b4468df9fd961f5015ee40a8
AlviM <adormundo@gmail.com>
2024-07-27 01:12:52 -0300

    👨🏻‍💻 Aula: 29

---
+++ src/main/java/com/github/trainingcoder/controle/C037_Continue.java
@@ -0,0 +1,14 @@
+package main.java.com.github.trainingcoder.controle;
+
+public class C037_Continue {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1) {
+                continue; // Interrompe a interação, mas segue para proxima
+            }
+            System.out.println(i);
+        }
+
+    }
+}
