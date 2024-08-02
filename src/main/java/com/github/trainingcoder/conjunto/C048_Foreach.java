fccba4dedb2bd879f537ddf20eed320db93200d3
AlviM <adormundo@gmail.com>
2024-08-01 21:18:32 -0300

    👨🏻‍💻 Aula: 40

---
+++ src/main/java/com/github/trainingcoder/conjunto/C048_Foreach.java
@@ -0,0 +1,11 @@
+package main.java.com.github.trainingcoder.conjunto;
+
+public class C048_Foreach {
+    public static void main(String[] args) {
+        double[] notas = {6.9, 8.9, 5.5, 10};
+
+        for (double nota : notas) {
+            System.out.println(nota);
+        }
+    }
+}
