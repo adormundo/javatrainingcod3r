0bff615a38d26134e18e54e1befa830e74f04ff0
AlviM <adormundo@gmail.com>
2024-08-05 23:45:06 -0300

    👨🏻‍💻 Aula: 47

---
+++ src/main/java/com/github/trainingcoder/conjunto/C055_Pilha.java
@@ -0,0 +1,25 @@
+package main.java.com.github.trainingcoder.conjunto;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class C055_Pilha {
+	public static void main(String[] args) {
+		Deque<String> livros = new ArrayDeque<>();
+
+		
+		// Adiciona elemento na lista, se exceder limite retorna bool
+		livros.add("O Pequeno Príncipe");
+		
+		// Adiciona elemento na lista, se exceder limite retorna excpetion
+		livros.push("Don Quixote");
+		livros.push("O Hobbit");
+		
+		
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+		System.out.println(livros.poll());
+		System.out.println(livros.pop());
+		System.out.println(livros.remove());
+	}
+}
