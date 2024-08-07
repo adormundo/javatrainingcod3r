9c322a4a1ab17cf6d5137dcd65b083810c023a25
AlviM <adormundo@gmail.com>
2024-08-07 19:46:32 -0300

    👨🏻‍💻 Aula: 49

---
+++ src/main/java/com/github/trainingcoder/oo/composicao/C057_Carro.java
@@ -0,0 +1,25 @@
+package main.java.com.github.trainingcoder.oo.composicao;
+
+public class C057_Carro {
+	final C057_Motor motor = new C057_Motor();
+	
+	void acelerar() {
+		if (motor.fatorInjecao < 2.6) motor.fatorInjecao += 0.4;
+	}
+	
+	void frear() {
+		if (motor.fatorInjecao > 0.5) motor.fatorInjecao -= 0.4;
+	}
+	
+	void ligar() {
+		motor.ligado = true;
+	}
+	
+	void desligar() {
+		motor.ligado = false;
+	}
+	
+	boolean estaLigado() {
+		return motor.ligado;
+	}
+}
---
+++ src/main/java/com/github/trainingcoder/oo/composicao/C057_Motor.java
@@ -0,0 +1,15 @@
+package main.java.com.github.trainingcoder.oo.composicao;
+
+public class C057_Motor {
+
+	boolean ligado = false;
+	double fatorInjecao = 1;
+
+	int giros() {
+		if (!ligado) {
+			return 0;
+		} else {
+			return (int) Math.round(fatorInjecao * 3000);
+		}
+	}
+}
