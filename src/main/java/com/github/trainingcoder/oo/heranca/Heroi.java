bd4b3d7b8f85b36451cd9c064680d92d3d5cd011
AlviM <adormundo@gmail.com>
2024-08-09 21:28:10 -0300

    👨🏻‍💻 Aula: 55

--- src/main/java/com/github/trainingcoder/oo/heranca/Heroi.java
+++ src/main/java/com/github/trainingcoder/oo/heranca/Heroi.java
@@ -2,5 +2,11 @@ package main.java.com.github.trainingcoder.oo.heranca;
 
 public class Heroi extends Jogador {
 	
+	boolean atacar(Jogador outroJogador) {
+		boolean ataque1 = super.atacar(outroJogador);
+		boolean ataque2 = super.atacar(outroJogador);
+		boolean ataque3 = super.atacar(outroJogador);
+		return ataque1 || ataque2 || ataque3;
+	}
 
 }
--- src/main/java/com/github/trainingcoder/oo/heranca/Jogo.java
+++ src/main/java/com/github/trainingcoder/oo/heranca/Jogo.java
@@ -3,22 +3,23 @@ package main.java.com.github.trainingcoder.oo.heranca;
 public class Jogo {
 
 	public static void main(String[] args) {
-		Jogador jogador1 = new Monstro();
-		jogador1.x = 10;
-		jogador1.y = 10;
+		Jogador monstro = new Monstro();
+		monstro.x = 10;
+		monstro.y = 10;
 		
-		Jogador jogador2 = new Heroi();
-		jogador2.x = 10;
-		jogador2.y = 11;
+		Jogador heroi = new Heroi();
+		heroi.x = 10;
+		heroi.y = 11;
 
-		System.out.printf("Life %d | Posicao X:%d Y:%d %n", jogador1.vida, jogador1.x, jogador1.y);
-		System.out.printf("Life %d | Posicao X:%d Y:%d %n", jogador2.vida, jogador2.x, jogador2.y);
+		System.out.printf("[M] => Vida %d | Posicao X:%d Y:%d %n", monstro.vida, monstro.x, monstro.y);
+		System.out.printf("[H] => Vida %d | Posicao X:%d Y:%d %n", heroi.vida, heroi.x, heroi.y);
 		System.out.println();
 		
-		jogador1.atacar(jogador2);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
 		
-		System.out.printf("Life %d | Posicao X:%d Y:%d %n", jogador1.vida, jogador1.x, jogador1.y);
-		System.out.printf("Life %d | Posicao X:%d Y:%d %n", jogador2.vida, jogador2.x, jogador2.y);
+		System.out.printf("[M] => Vida %d | Posicao X:%d Y:%d %n", monstro.vida, monstro.x, monstro.y);
+		System.out.printf("[H] => vida %d | Posicao X:%d Y:%d %n", heroi.vida, heroi.x, heroi.y);
 		System.out.println();
 		
 		
