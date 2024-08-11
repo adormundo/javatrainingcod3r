6367065cc9f2e45808d60942ea629ad780396439
AlviM <adormundo@gmail.com>
2024-08-11 15:28:40 -0300

    👨🏻‍💻 Aula: 66

--- src/main/java/com/github/trainingcoder/oo/heranca/desafio/Luxo.java
+++ src/main/java/com/github/trainingcoder/oo/heranca/desafio/Luxo.java
@@ -3,4 +3,8 @@ package main.java.com.github.trainingcoder.oo.heranca.desafio;
 public interface Luxo {
 	void ligarAr();
 	void desligarAr();
+	
+	 default int velocidadeDoAr() {
+		 return 1;
+	 }
 }
