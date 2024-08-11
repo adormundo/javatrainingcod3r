086b74be261fd5b095f99ac142a2f01c864ae60d
AlviM <adormundo@gmail.com>
2024-08-11 16:47:07 -0300

    👨🏻‍💻 Aula: 68

--- src/main/java/com/github/trainingcoder/lambdas/Calculo.java
+++ src/main/java/com/github/trainingcoder/lambdas/Calculo.java
@@ -1,5 +1,7 @@
 package main.java.com.github.trainingcoder.lambdas;
 
+//Essa annotation força ter um método por arquivo
+@FunctionalInterface
 public interface Calculo {
 	public abstract double executar(double a, double b);
 	
